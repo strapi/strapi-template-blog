e809f38ab6a7f2f696ca833947d3c7bd3e3891dd
Rémi de Juvigny <remi@hey.com>
2020-10-08 14:46:43 +0200

    Setup for Heroku

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe2a41dd8ea34db4a579dd087441b482'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9445f9b9-de89-4ccc-b1e9-7fbf9721f1ef'
+};
